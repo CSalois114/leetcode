958b7230064129dc27dd54ae1f001901bb00dfc6
Chris Salois <47462756+CSalois114@users.noreply.github.com>
2022-08-25 15:59:09 -0400

    Time: 92 ms (57.46%), Space: 44.9 MB (6.66%) - LeetHub

---
+++ 206-reverse-linked-list/206-reverse-linked-list.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    if(!head?.next) return head
+    let final = reverseList(head.next)
+    let temp = head.next
+    head.next = null
+    temp.next = head
+
+    return final
+};
