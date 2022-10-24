3d7d487e895f8b156a20ad0dc56403993c3acc25
Chris Salois <47462756+CSalois114@users.noreply.github.com>
2022-10-24 17:01:47 -0400

    Time: 107 ms (29.77%), Space: 42.3 MB (89.37%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.js
@@ -4,40 +4,31 @@
  * @return {number[]}
  */
 var searchRange = function(nums, target) {
-  let output = [-1, -1]
+  const output = [-1, -1]
   
   const recBin = (start, end, direction=null) => {
     const mid = Math.floor((start + end) / 2);
     if(start > end) return;
     
-    if(nums[mid] == target) {
-      if(direction) {
-        
-        if(direction == "LEFT") {
-          output[0] = mid
-          recBin(start, mid - 1, direction)
-        } else {
-          output[1] = mid
-          recBin(mid + 1, end, direction)
-        } 
-        
-      } else {
-        
-        output = [mid, mid]
+    if(nums[mid] == target) {  
+      if(direction == "LEFT") {
+        output[0] = mid
+        recBin(start, mid - 1, direction)
+      } else if(direction == "RIGHT") {
+        output[1] = mid
+        recBin(mid + 1, end, direction)
+      } else { 
+        output[0] = output[1] = mid
         recBin(start, mid - 1, "LEFT")
-        recBin(mid + 1, end, "RIGHT")
-        
+        recBin(mid + 1, end, "RIGHT")  
       }
-      
     } else {
-      
       if(nums[mid] < target) {
         recBin(mid + 1, end, direction)
       } else {
         recBin(start, mid - 1, direction)
       }
-      
-    }  
+    }
   }
   
   recBin(0, nums.length)
