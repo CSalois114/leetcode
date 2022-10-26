3d6882c1c7d2e800660a466ba32efebe3b3cd8d7
Chris Salois <47462756+CSalois114@users.noreply.github.com>
2022-10-26 14:34:23 -0400

    Time: 108 ms (28.05%), Space: 41.8 MB (91.18%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let start = 0
+    let end = nums.length - 1
+    let mid
+    
+    while(start <= end) {
+      mid = Math.floor((start + end) / 2)
+      if(nums[mid] == target) return mid  
+      
+      if(nums[end] >= target) {
+        if(nums[mid] > target && nums[mid] < nums[end]) {
+          end = mid - 1
+        } else {
+          start = mid + 1
+        }
+      } else {
+        if(nums[mid] > target || nums[mid] < nums[end]) {
+          end = mid - 1
+        } else {
+          start = mid + 1
+        }
+      }
+    }
+  
+    return -1
+};
