29dc4f830dfb3007c16550a0dbd436653dc2b395
Chris Salois <47462756+CSalois114@users.noreply.github.com>
2022-10-26 15:12:53 -0400

    Time: 98 ms (51.54%), Space: 42.2 MB (54.56%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -7,23 +7,19 @@ var search = function(nums, target) {
     let start = 0
     let end = nums.length - 1
     let mid
-    
+        
     while(start <= end) {
       mid = Math.floor((start + end) / 2)
       if(nums[mid] == target) return mid  
       
-      if(nums[end] >= target) {
-        if(nums[mid] > target && nums[mid] < nums[end]) {
-          end = mid - 1
-        } else {
-          start = mid + 1
-        }
+      if(
+        nums[end] >= target ? 
+        nums[mid] > target && nums[mid] < nums[end] :
+        nums[mid] > target || nums[mid] < nums[end]
+      ) {          
+        end = mid - 1
       } else {
-        if(nums[mid] > target || nums[mid] < nums[end]) {
-          end = mid - 1
-        } else {
-          start = mid + 1
-        }
+        start = mid + 1
       }
     }
   
