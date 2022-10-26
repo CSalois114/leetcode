b8a4a06def7a29c61967504447a2b32a7983b7cf
Chris Salois <47462756+CSalois114@users.noreply.github.com>
2022-10-26 18:02:59 -0400

    Time: 76 ms (81.90%), Space: 41.6 MB (97.86%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -3,26 +3,20 @@
  * @param {number} target
  * @return {boolean}
  */
-var searchMatrix = function(mtrx, trgt) {
+var searchMatrix = function(matrix, target) {
+  let columnSize = matrix.length
+  let rowSize = matrix[0].length
+  
   let low = 0;
-  let high = mtrx.length - 1
-  let mid, arr;
+  let high = (columnSize * rowSize) - 1
+  let mid, val;
   
   while(low <= high) {
     mid = Math.floor((low + high) / 2)
-    if(arr) {
-      if(arr[mid] == trgt) return true
-      arr[mid] >= trgt ? high = mid - 1 : low = mid + 1
-    } else {
-      if(mtrx[mid][0] <= trgt && mtrx[mid][mtrx[mid].length - 1] >= trgt) {
-        arr = mtrx[mid]
-        low = 0
-        high = mtrx[mid].length - 1
-      } else {
-        mtrx[mid][0] >= trgt ? high = mid - 1 : low = mid + 1
-      }
-    } 
+    val = matrix[Math.floor(mid / rowSize)][mid % rowSize]
+    if(val == target) return true
+    val > target ? high = mid - 1 : low = mid + 1
   }
-
+  
   return false
 };
