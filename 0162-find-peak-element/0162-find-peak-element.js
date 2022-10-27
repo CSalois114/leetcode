df5cb82578157d643c8d2114063de115b27edef6
Chris Salois <47462756+CSalois114@users.noreply.github.com>
2022-10-27 15:38:22 -0400

    Time: 98 ms (43.84%), Space: 41.8 MB (84.16%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function(nums) {
+  let left = 0
+  let right = nums.length - 1
+  let mid;
+  
+  while(left <= right) {
+    mid = Math.floor((left + right) / 2)
+    if(nums[mid - 1] > nums[mid]){
+      right = mid - 1
+    } else if(nums[mid + 1] > nums[mid]) {
+      left = mid + 1
+    } else {
+        return mid
+    }
+  }
+};
